80a70563d517d0ac73c3153f6a2299fa1fafb3e6
InvisUA <xndell001@gmail.com>
2024-10-13 15:31:47 +0300

    first commit

---
+++ test_nba_api.py
@@ -0,0 +1,18 @@
+from nba_api.stats.static import teams
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+def one_dict(list_dict):
+    keys=list_dict[0].keys()
+    out_dict={key:[] for key in keys}
+    for dict_ in list_dict:
+        for key, value in dict_.items():
+            out_dict[key].append(value)
+    return out_dict
+
+nba_teams = teams.get_teams()
+dict_nba_team=one_dict(nba_teams)
+df_teams=pd.DataFrame(dict_nba_team)
+
+print(df_teams)

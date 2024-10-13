26983324635798082990161c2e9c072f243ca963
InvisUA <xndell001@gmail.com>
2024-10-13 11:10:41 +0300

    first commit

---
+++ pycoingecko.py
@@ -0,0 +1,10 @@
+from pycoingecko import CoinGeckoAPI
+import pandas as pd
+
+cg = CoinGeckoAPI()
+bitcoin_data = cg.get_coin_market_chart_by_id(id = 'bitcoin', vs_currency = 'usd', days = 30)
+data = pd.DataFrame(bitcoin_data['prices'], columns=['TimeStamp', 'Price'])
+
+data['Date'] = pd.to_datetime(data['TimeStamp'], unit='ms')
+candlestick_data = data.groupby(data.Date.dt.date).agg({'Price':['min', 'max', 'first', 'last']})
+print(candlestick_data)
